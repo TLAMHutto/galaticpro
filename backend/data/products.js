0cdc42461e44fdb065a8b26bafef7e0b44b71c43
TLAMHutto <keatonhutto8@gmail.com>
2024-09-28 01:59:12 -0500

    images

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'EOCS (Emergency Oxygen Containment System)',
+    image: 'https://i.pinimg.com/564x/a0/ce/a1/a0cea14d74aab7a021a09182e4712fd2.jpg',
+    description:
+      'The Emergency Oxygen Containment System (EOCS) is a critical life-support apparatus designed to ensure the availability of breathable oxygen during emergencies aboard spacecraft. This system is engineered to function seamlessly in the harsh conditions of space, providing astronauts with immediate access to oxygen in the event of a cabin depressurization or other critical situations.',
+    brand: 'MDI',
+    category: 'Supplies',
+    price: 3489.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'The Cosmic Travelers Handbook',
+    image: 'https://i.pinimg.com/564x/e6/61/65/e66165cb1930d0f3067fb09e63ac08dc.jpg',
+    description:
+      'In a universe filled with bizarre creatures, absurd bureaucracies, and mind-bending physics, "The Cosmic Travelers Handbook" is your essential guide to navigating the vast and unpredictable cosmos. Dont travel interstellar space without one',
+    brand: 'Zoltran',
+    category: 'Supplies',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'NebulaGrip',
+    image: 'https://i.pinimg.com/736x/a6/a5/0f/a6a50f60a6c26af24e1314f7f6acbab3.jpg',
+    description:
+      'The NebulaGrip Glove is the pinnacle of advanced technology in personal space gear, designed for the intrepid space explorer navigating the challenges of the cosmos. Melding cutting-edge materials with ergonomic design, these gloves offer unparalleled dexterity and protection for a wide range of space missions.',
+    brand: 'Nebula Corp',
+    category: 'Suit',
+    price: 1929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'AstroVision AR Specs',
+    image: 'https://i.pinimg.com/564x/1d/62/91/1d6291eb05520c0a857d5c8c8aae2a58.jpg',
+    description:
+      'The AstroVision AR Specs are the next generation of augmented reality glasses specifically designed for space exploration and spaceship operations. These cutting-edge glasses empower astronauts and engineers with real-time data visualization and interactive interfaces, making spaceship operation, maintenance, and troubleshooting more intuitive and efficient than ever before.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 2399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'TRNTL Controller',
+    image: 'https://i.pinimg.com/564x/8e/e6/f4/8ee6f40adcab92ae2b5f23f1e7ff3355.jpg',
+    description:
+      'TRNTL Controller is a state-of-the-art control interface designed for the seamless operation of external ship robots in a variety of space environments. This advanced controller empowers astronauts and technicians to efficiently manage robotic units for maintenance, exploration, and emergency response tasks outside the spacecraft.',
+    brand: 'MaviTech',
+    category: 'Electronics',
+    price: 9949.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'AstroMed Portable First Aid Kit',
+    image: 'https://i.pinimg.com/564x/86/a8/b6/86a8b66c6b1ba567f5bd6b464e7f772f.jpg',
+    description:
+      'The AstroMed Portable First Aid Kit is an essential companion for space travelers and adventurers, meticulously designed to provide immediate medical assistance in the challenging environments of outer space. Compact, lightweight, and packed with advanced medical supplies, this first aid kit ensures that astronauts are prepared to handle a variety of medical emergencies, from minor injuries to more critical situations.',
+    brand: 'AMS',
+    category: 'Supplies',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
